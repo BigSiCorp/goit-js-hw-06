dab4a19b38def56b2f502575c3bfd7e102ad8959
BigSiCorp <si.kocorp@gmail.com>
2022-08-25 23:44:35 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+const generateColor = document.querySelector(".change-color")
+const body = document.body
+const span = document.querySelector(".color")
+
+generateColor.addEventListener('click', getRandomHexColor)
+generateColor.addEventListener('click', changeColor)
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
