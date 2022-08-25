acd8d8912ee7086340b40b143646ae2223d9a033
BigSiCorp <si.kocorp@gmail.com>
2022-08-25 22:37:14 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const mail = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    const formData = {
+        mail,
+        password,
+    };
+
+    if (mail === '') {
+        alert('Fill the row with email, please');
+    } else if (password === '') {
+        alert('Enter your password, please');
+    } else {
+        console.log(formData);
+        form.reset();
+}
+};
+
+
+
 
